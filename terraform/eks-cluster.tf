4906b22f5ff30ecb2808956f0ac77dc13ac78b68
ravex23 <reminiscing_23@hotmail.com>
2026-01-21 18:57:39 +0500

    fmt: terraform format

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -6,8 +6,8 @@ module "eks" {
   name               = local.cluster_name
   kubernetes_version = "1.34"
 
-  vpc_id                = module.vpc.vpc_id
-  subnet_ids            = module.vpc.private_subnets
+  vpc_id                 = module.vpc.vpc_id
+  subnet_ids             = module.vpc.private_subnets
   endpoint_public_access = true
 
   # EKS Managed Node Group(s)
