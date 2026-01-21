b4fe5bb9f22fce6baa55dd9f1e06ce45638a6a6d
ravex23 <reminiscing_23@hotmail.com>
2026-01-21 18:22:00 +0500

    Bucket name & cluster name updated

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,23 +1,22 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.19.1"
+  version = "21.15.1"
 
-  cluster_name    = local.cluster_name
-  cluster_version = "1.27"
+  # v21.x input names
+  name               = local.cluster_name
+  kubernetes_version = "1.34"
 
-  vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
-  cluster_endpoint_public_access = true
+  vpc_id                = module.vpc.vpc_id
+  subnet_ids            = module.vpc.private_subnets
+  endpoint_public_access = true
 
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-
-  }
-
+  # EKS Managed Node Group(s)
   eks_managed_node_groups = {
     one = {
       name = "node-group-1"
 
+      # Starting on 1.30, AL2023 is the default for EKS managed node groups
+      ami_type       = "AL2023_x86_64_STANDARD"
       instance_types = ["t3.small"]
 
       min_size     = 1
@@ -28,6 +27,7 @@ module "eks" {
     two = {
       name = "node-group-2"
 
+      ami_type       = "AL2023_x86_64_STANDARD"
       instance_types = ["t3.small"]
 
       min_size     = 1
